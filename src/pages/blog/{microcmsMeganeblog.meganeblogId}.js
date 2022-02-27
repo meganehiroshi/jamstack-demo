c6cff429f709544ed4adafcc72006e274948dcd0
hiroshihiroshi <kawasakhiroshi@gmail.com>
2022-02-27 15:57:12 +0900

    jqueryで動いているjsのinclude

--- src/pages/blog/{microcmsMeganeblog.meganeblogId}.js
+++ src/pages/blog/{microcmsMeganeblog.meganeblogId}.js
@@ -2,10 +2,16 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../../components/layout"
+import Helmet from "react-helmet"
 import SEO from "../../components/seo"
+import { withPrefix, Link } from "gatsby"
+
 
 const BlogPage = ({ data }) => (
   <Layout>
+    <Helmet>
+        <script src={withPrefix('../../../static/js/clova.js')} type="text/javascript" />
+    </Helmet>
     <SEO title={data.microcmsMeganeblog.title} />
     <span>{data.microcmsMeganeblog.date}</span>
     <h1>{data.microcmsMeganeblog.title}</h1>
